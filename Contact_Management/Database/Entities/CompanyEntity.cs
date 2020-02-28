442e90ff698cef90a32a937882899a38d1dfb7ef
pkriwin <pkriwin@caami-hziv.fgov.be>
2020-02-28 14:52:33 +0100

    added entities

---
+++ Contact_Management/Database/Entities/CompanyEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contact_Management.Database.Entities
+{
+    public class CompanyEntity
+    {
+        public string Name { get; set; }
+        public string VATIdNumber { get; set; }
+        public string HeadQuarterAddress { get; set; }
+        public string OtherAdresses { get; set; } // Semi colon delimited
+    }
+}
---
+++ Contact_Management/Database/Entities/ContactEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Contact_Management.Database.Entities
+{
+    public class ContactEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public ContactType Type { get; set; }
+
+        // Freelance Contacts fields
+        public string VATIdNumber { get; set; }
+
+        public enum ContactType
+        {
+            Employee,
+            Freelancer
+        }
+    }
+}
