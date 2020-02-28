7e30b12ab36ec1e32687aa744bd7ecec95b2cc1c
pkriwin <pkriwin@caami-hziv.fgov.be>
2020-02-28 12:26:25 +0100

    init solution

---
+++ Contact_Management/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Contact_Management.Controllers
+{
+    [ApiController]
+    [Route("contacts")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ILogger<ContactsController> _logger;
+
+        public ContactsController(ILogger<ContactsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
